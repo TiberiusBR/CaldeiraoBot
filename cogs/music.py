3a1791d46b0d55c86ba5479fad5b388a8d08ad66
TiberiusBR <souza.freire@ftc.edu.br>
2023-02-05 10:42:36 -0300

    refactor: change music provider to wavelink

--- cogs/music.py
+++ cogs/music.py
@@ -1,98 +1,142 @@
 import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction
-from helpers.ytdl import YTDLSource
 
-import asyncio
+import datetime
+
+import wavelink
+
 from helpers.logger import logger
 
+
+class CustomPlayer(wavelink.Player):
+
+    def __init__(self):
+        super().__init__()
+        self.queue = wavelink.Queue()
+
+
 class Music(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
-        self.queues = {}
-    
-    def check_queue(self,voice_client, guild_id: int):
-        if self.queues[guild_id] != []:
-            voice = voice_client
-            source = self.queues[guild_id].pop(0)
-            player = voice.play(source)
-            logger.info(f"Current song for guild {guild_id}: {source.title}")
-    
-    def clear_queue(self, guild_id):
-        if guild_id in self.queues:
-            self.queues[guild_id].clear()
-            logger.info(f"Queue for {guild_id} cleared.")
+        self.info_queues = {}
 
-    async def add_to_queue(self, interaction: Interaction, url: str):
-        """Adds a youtube video to the queue."""
-        
-        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-        guild_id = interaction.guild_id
-        if guild_id in self.queues:
-            self.queues[guild_id].append(player)
-        else:
-            self.queues[guild_id] = [player]
-        return await interaction.send(f"Added {player.title} to queue.")
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, player: CustomPlayer, track: wavelink.Track, reason):
+        if not player.queue.is_empty:
+            next_track = player.queue.get()
+            await player.play(next_track)
 
-    ##@play.before_invoke()
+    def parse_time(self, duration: float):
+        return str(datetime.timedelta(seconds=duration))
+
     async def ensure_voice(self, interaction: Interaction):
         voice_client = interaction.guild.voice_client
         if voice_client is None:
             if interaction.user.voice:
-                voice_client = await interaction.user.voice.channel.connect()
+                voice_client: CustomPlayer = await interaction.user.voice.channel.connect(cls=CustomPlayer())
             else:
                 await interaction.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
+                raise commands.CommandError(
+                    "Author not connected to a voice channel.")
         return voice_client
-    
-    @nextcord.slash_command(name="join",description="Joins current user voice channel.")
+
+    @nextcord.slash_command(name="join", description="Joins current user voice channel.")
     async def join(self, interaction: Interaction):
         voice_channel = interaction.user.voice.channel
-        if voice_channel is None:
-            await interaction.send(f"You aren't connected to any channel.")
-
-        await voice_channel.connect()
+        await self.ensure_voice(interaction)
         await interaction.send(f"Connected to channel {voice_channel.name}")
-    
-    @nextcord.slash_command(name="play",description="Play a video from a youtube URL.")
-    async def play(self, interaction: Interaction, url):
+
+    @nextcord.slash_command(name="play", description="Play a video from a youtube URL or query.")
+    async def play(self, interaction: Interaction, search: str):
         """Streams from a URL (same as yt, but doesn't predownload)"""
         try:
-            await interaction.response.defer()
             voice_client = await self.ensure_voice(interaction)
-            if voice_client.is_playing():
-                return await self.add_to_queue(interaction,url)
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            video_title = player.title
-            voice_client.play(
-                player, after=lambda x=None: self.check_queue(voice_client,interaction.guild.id)
-            )
-
-            await asyncio.sleep(0.1)
-            await interaction.followup.send(f"Now playing: {video_title}")
+            embed = None
+            if "playlist" in search:
+                search = await wavelink.YouTubePlaylist.search(query=search)
+                tracks: list = search.tracks
+                track_amount = len(tracks)
+                await voice_client.play(tracks.pop(0))
+                for track in tracks:
+                    voice_client.queue.put(track)
+                embed = nextcord.Embed(title="Playlist enqueued!",
+                                       description=f"Added playlist **{search.name}**, with **{track_amount}** tracks to queue!")
+            else:
+                if "&" in search and "youtube" in search:
+                    search = search.split("&")[0]
+                search = await wavelink.YouTubeTrack.search(query=search, return_first=True)
+                if voice_client.is_playing():
+                    voice_client.queue.put(search)
+                    embed = nextcord.Embed(title=search.title,
+                                           url=search.uri,
+                                           description=f"Added **{search.title}** to queue!")
+                else:
+                    await voice_client.play(search)
+                    embed = nextcord.Embed(title=search.title,
+                                           url=search.uri,
+                                           description=f"Playing **{search.title}**!")
+            await interaction.send(embed=embed)
         except Exception as ex:
-            logger.error(f"A exception ocurred: {ex.message}")
+            logger.error(f"A exception ocurred: {ex}")
             await interaction.followup.send("The video couldn't be played. Try again.")
-    
+
+    @nextcord.slash_command(name="queue", description="Retrieve all songs currently on the server queue.")
+    async def queue(self, interaction: Interaction):
+        """Retrieve songs from queue"""
+        empty_msg = ""
+        message = f"All songs currently in queue for **{interaction.guild.name}**: \n >>> "
+        try:
+            try:
+                queue_obj = interaction.guild.voice_client.queue
+                queue_size = queue_obj.count
+                queue = queue_obj._queue
+                queue_messages = []
+                if queue_size > 0:
+                    for idx, song in enumerate(queue):
+                        if idx <= 9:
+                            queue_messages.append(f"**{song.title}** - `({self.parse_time(song.duration)})`\n")
+                        else:
+                            empty_msg = f"And more {queue_size - idx} songs in queue!"
+                            break
+                else:
+                    message += "**There are currently no songs in queue.**"
+                message += "".join(queue_messages)
+                message += empty_msg
+            except:
+                logger.error(f"A exception ocurred: {ex}")
+            await interaction.send(message)
+        except Exception as ex:
+            logger.error(f"A exception ocurred: {ex}")
+            await interaction.followup.send("Could not check queue. Try again.")
+
     @nextcord.slash_command(name="stop", description="Disconnect bot from voice channel and clear queue, if there's one.")
     async def stop(self, interaction: Interaction):
         """Stops and disconnects the bot from voice"""
         voice_client = interaction.guild.voice_client
-        self.clear_queue(interaction.guild_id)
+        queue = voice_client.queue
+        await voice_client.stop()
         voice_client.cleanup()
         await voice_client.disconnect()
-    
+        queue.clear()
+        await interaction.send("Queue cleared and disconnected from voice channel.")
+
     @nextcord.slash_command(name="skip", description="Plays the next song in queue, if there's any.")
     async def skip(self, interaction: Interaction):
         """Skips current song."""
-        voice_protocol = interaction.guild.voice_client
-        if voice_protocol != None:
-            if voice_protocol.is_playing():
+        voice_client = interaction.guild.voice_client
+        if voice_client:
+            if not voice_client.is_playing():
                 voice_client = interaction.guild.voice_client
                 video_title = voice_client.source.title
                 voice_client.stop()
                 await interaction.send(f"Skipping {video_title}")
-            else:
-                await interaction.send("There's no songs left in the queue.")
+            if voice_client.queue.is_empty:
+                await voice_client.stop()
+                await interaction.send("There's no songs left in the queue. Stopping current song.")
+
+            await voice_client.seek(voice_client.track.length * 1000)
+            if voice_client.is_paused():
+                await voice_client.resume()
         else:
-            await interaction.send("Not connected to any channel.")
+            await interaction.send("Not connected to any voice channel.")
--- index.py
+++ index.py
@@ -4,19 +4,12 @@ from dotenv import load_dotenv
 from nextcord.ext import commands
 
 from helpers.logger import logger
+
 from cogs.music import Music
 from cogs.misc import Misc
+from cogs.chat_gpt import ChatGPT
 
-import asyncio
-
-from fastapi import FastAPI
-import uvicorn
-
-#app = FastAPI()
-
-# @app.get("/")
-# async def health():
-#     return {"Health": "Ok!"}
+import wavelink
 
 load_dotenv()
 
@@ -25,20 +18,23 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
 @bot.event
 async def on_ready():
     logger.info('Ready!')
+    bot.loop.create_task(node_connect())
+
+
+async def node_connect():
+    await bot.wait_until_ready()
+    await wavelink.NodePool.create_node(bot=bot,
+                                           host=str(os.getenv('LAVALINK_HOST')),
+                                           port=str(os.getenv('LAVALINK_PORT')),
+                                           password=str(os.getenv('LAVALINK_PASSWORD')),
+                                           region="brazil")
 
 bot.add_cog(Music(bot))
 bot.add_cog(Misc(bot))
+bot.add_cog(ChatGPT(bot))
 
 bot.run(os.getenv('BOT_TOKEN'))
-
-# @app.on_event("startup")
-# async def startup_event():
-#     asyncio.create_task(bot.start(os.getenv('BOT_TOKEN')))
-#     await asyncio.sleep(4) #optional sleep for established connection with discord
-#     logger.info(f"{bot.user} has connected to Discord!")
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, port=8000)
